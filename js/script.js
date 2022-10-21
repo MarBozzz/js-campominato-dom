5e3fc595e2d048000eb38599853cb4b461f20dd5
MarBozzz <mbozzuto@libero.it>
2022-10-21 16:18:48 +0200

    [added] bombs array in js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
 /*
 **Consegna**
-L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-**Bonus**
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+****L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.**BONUS:**
+1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
 */
+
+
+//generare bombe
+
+//al click square
+//se clicko bomba Game over
+//contatore di tentativi
+//con tentativi finiti Game over
+
+//Game over funzione
+//stampare risultato
+//freeze del container tramite overlay
+
+//reset funzione
+//genero nuove bombe
 
 const container = document.querySelector('.container');
 let sqPerRow;
+
+const BOMB_NUMBER = 16;
+let bombs = [];
 
 document.getElementById('start').addEventListener('click', function(){
   sqPerRow = document.getElementById('level').value;
@@ -29,6 +47,8 @@ function init (ElPerRow){
   for(let i = 0; i < totalSquares; i++) {
     createSquares(i);
   }
+  bombs = addBombs(totalSquares);
+  console.log('bombs', bombs);
 }
 
 function createSquares (idSquare){
@@ -41,6 +61,26 @@ function createSquares (idSquare){
   square.customSq = idSquare + 1;
   square.addEventListener('click', clickedSq)
   container.append(square);
+}
+
+function addBombs (totalSquares){
+  //array di bombs da riempire fino al raggiungimento di 16
+  const addedBombs = [];
+  //con ciclo while genero le bombs
+  while (addedBombs.length < BOMB_NUMBER) {
+    //faccio estrarre un numero da 1 al numero totale di squares  
+    const bomb = getRandomNumber(1, totalSquares);
+    //se l'estratto non è presente nell'array si pusha dentro
+    if(!addedBombs.includes(bomb)){
+    addedBombs.push(bomb);
+    }
+  }
+
+  return addedBombs;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function clickedSq() {
